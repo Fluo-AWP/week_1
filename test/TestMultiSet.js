f4d24e53f57de341d706c2899eced04dd23dfde7
Hagbarth <spjerx@gmail.com>
2014-11-09 15:02:30 +0100

    removed qunit and added fluo unit test

--- test/TestMultiSet.js
+++ test/TestMultiSet.js
@@ -1,30 +1,29 @@
 var testObject = {'test': true};
 
-test('Test contains returns boolean', function() {
+test('MultiSet.contains() should return boolean value', function() {
 	var multiset = new MultiSet();
-
-	ok(typeof multiset.contains(testObject) === 'boolean', 'Should be boolean');
+	ok(typeof multiset.contains(testObject) === 'boolean', 'Returned value should be boolean');
 });
 
-test('Test count returns number', function() {
+test('MultiSet.contains() should return number', function() {
 	var multiset = new MultiSet();
 
-	ok(typeof multiset.count(testObject) === "number", 'Should be numeric');
+	ok(typeof multiset.count(testObject) === "number", 'Returned value should be numeric');
 });
 
 test('Test can add to multiset', function() {
 	var multiset = new MultiSet();
 
 	multiset.add(testObject);
-	equal(multiset.count(testObject), 1, 'Multiset count should return 1');
-	ok(multiset.contains(testObject), 'Multiset should contain object: {"test": true}');
+	equal(multiset.count(testObject), 1, 'Added one testObject and called multiset.count(testObject): Multiset count should return 1');
+	ok(multiset.contains(testObject), 'Called multiset.contains(testObject): Should return true');
 
 	multiset.add(testObject);
-	equal(multiset.count(testObject), 2, 'Multiset count should return 2');
+	equal(multiset.count(testObject), 2, 'Added another testObject and called multiset.count(testObject): Multiset count should now return 2');
 
 	multiset.add("test");
-	equal(multiset.count(testObject), 2, 'Multiset count should return 2');
-	equal(multiset.count("test"), 1, 'Multiset count should return 1');
+	equal(multiset.count(testObject), 2, 'Added "test" to multiset and called multiset.count(testObject). Multiset count should still return 2');
+	equal(multiset.count("test"), 1, 'Called multiset.count("test). Multiset count should return 1');
 });
 
 test('Test can remove from multiset', function() {
@@ -33,9 +32,9 @@ test('Test can remove from multiset', function() {
 	multiset.add(testObject);
 	multiset.add(testObject);
 	multiset.remove(testObject);
-	equal(multiset.count(testObject), 1, 'Multiset count should return 0');
+	equal(multiset.count(testObject), 1, 'Added two testObjects and removed one. Called multiset.count(testObject): Multiset count should return 1');
 	multiset.remove(testObject);
-	equal(multiset.count(testObject), 0, 'Multiset count should return 0');
+	equal(multiset.count(testObject), 0, 'Removed the other testObject. Called multiset.count(testObject): Multiset count should return 0');
 });
 
 test('Test prints multiset tostring correctly', function() {
@@ -44,6 +43,6 @@ test('Test prints multiset tostring correctly', function() {
 	multiset.add("test");
 	multiset.add(testObject);
 	multiset.add(testObject);
-	ok(typeof multiset.toString() === 'string', 'Multiset toString should return string');
-	equal(multiset.toString(), '[{"test": true} x 2, test]', 'Multiset toString should print [{"test": true} x 2, test]');
+	ok(typeof multiset.toString() === 'string', 'Tests if multiset.toString() returns a string');
+	equal(multiset.toString(), '[{"test": true} x 2, test]', 'Added two testObjects and one string: "test". Multiset toString should print [{"test": true} x 2, test]');
 });
